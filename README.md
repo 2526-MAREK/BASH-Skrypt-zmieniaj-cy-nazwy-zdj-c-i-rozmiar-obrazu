0ef18340493d776e3d7bbeb580e4614d3c2073b0
Marek Szulak <marek.szulak4@gmail.com>
2021-02-04 22:48:22 +0100

    Version_In_Working

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# BASH-Skrypt-zmieniaj-cy-nazwy-zdj-c-i-rozmiar-obrazu
+# Project Name
+Script BASH/SHELL Delete_space_and_choose_photo_size
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [How to use](#how-to-use)
+* [Status](#status)
+* [Contact](#contact)
+
+## General info
+This is script, which copies from chose one catalog all files with photos in format JPEG/PNG, change these file extensions on write small lettering, instead space in files name add "_", change size photo to entered argument in script and created archive with this files.
+
+## Technologies
+SHELL/BASH
+
+## How to use
+Being in folder with script enought in terminal(BASH/SHELL) enter: ./Script.sh and follow instruction(in Polish version).
+
+## Status
+Only Polish version(I will upgrade this, in the closest future).
+In working...
+
+## Contact
+Created by Marek Szulak
---
+++ Script.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+#Laboratorium 4 - Praca z uniksowym interpreterem poleceń oraz systemem X Window
+#MAREK_SZULAK, 259227
+
+
+
+#pytamy użytkownika
+echo "Jeśli twoje zdjęcia są w innym katalogu proszę wprowadz ścieżkę do niego (np Pulpit/TEST(Jeśli chcesz zostać w tym katalogu co jesteś proszę wpisz kropke (.):"
+read katalog
+echo "Proszę podaj do jakiego rozmiaru chcesz wyskalować wszystkie obrazy(np 100x200): "
+read rozmiar
+
+cd $katalog 
+
+#tworzymy katalog tymczasowy
+mkdir tymczasowy
+
+#kopiujemy pliki w formacie jpg i jpeg do katalogu tymczasowy 
+cp *jpeg tymczasowy
+cp *png tymczasowy
+cp *JPEG tymczasowy
+cp *PNG tymczasowy
+
+#przechodzimy do katalogu tymczasowy
+cd tymczasowy
+
+#zamiana spacji na podkreslnia w plikach z rozszerniem JPEG
+for file in *.JPEG
+do mv "$file"  "${file// /_}"
+done
+
+#zamiana spacji na podkreslenia w plikach z rozszerzeniem JPG
+for file in *.PNG
+do mv "$file"  "${file// /_}"
+done
+
+#zmiana duzych liter na male w rozszerzeniach plików
+for file in *JPEG
+do mv $file `basename $file .JPEG`.jpeg
+done
+
+for file in *PNG
+do mv $file `basename $file .PNG`.png
+done
+
+#Zmiana rozmiaru wszystkich obrazkow do tego samego rozmiaru
+for file in *jpeg
+do convert "$file" -resize "$rozmiar!" "$file"
+done
+
+for file in *png
+do convert "$file" -resize "$rozmiar!" "$file"
+done
+
+#tworzymy archiwum w danym katalogu ze wszystkich plików zakończonych png i jpeg
+tar -cvf archiwum.tar *png *jpeg
+
+mv archiwum.tar *jpeg *png cd ..
+
+cd ..
+
+rmdir tymczasowy
+
+#TESTY SKRYPTÓW:
+#1 Test
+#
+
+
+
+
+
